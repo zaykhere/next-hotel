d751052a653835b94abc8ad899c39abfa933e213
Zain Javed <zainjhere@gmail.com>
2022-04-06 00:49:57 +0500

    Handle mongoose path and validation error

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -5,6 +5,19 @@ export default (err,req,res,next) => {
     let error = {...err};
 
     error.message = err.message;
+
+    //Wrong mongooseId error
+    if(err.name === 'CastError') {
+        const message = `Resource not found. Invalid ${err.path}`;
+        error = new ErrorHandler(message, 400);
+    }
+
+    //Handling mongoose validation error
+    if(err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value=>value.message);
+        error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         error: error,
         message: error.message,
