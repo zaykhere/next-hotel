19f278bbbb2aa0a9c6d12050dc0997451fe793e2
Zain Javed <zainjhere@gmail.com>
2022-04-07 02:21:06 +0500

    Redux setup with roomsreducer,action and constants

---
+++ redux/actions/roomActions.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import absoluteUrl from "next-absolute-url";
+import { ALL_ROOMS_FAIL, ALL_ROOMS_SUCCESS, CLEAR_ERRORS } from "../constants/roomConstants";
+
+// Get all rooms action
+
+export const getRooms = (req) => async (dispatch) => {
+    try {
+        const {origin} = absoluteUrl(req);
+
+        const {data} = await axios.get(`${origin}/api/rooms`);
+        dispatch({type: ALL_ROOMS_SUCCESS, payload: data})
+        
+    } catch (error) {
+        console.log(error);
+        dispatch({type: ALL_ROOMS_FAIL, payload: error.response.data.message})
+    }
+}
+
+//CLEAR ERRORS
+
+export const clearErrors = () => async (dispatch) => {
+    dispatch({type: CLEAR_ERRORS})
+}
